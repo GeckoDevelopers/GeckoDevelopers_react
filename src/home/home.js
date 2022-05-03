5d7d4d79d6f2d449958503549ea9980db3189653
France900 <francesco.em.livio@gmail.com>
2022-05-03 15:31:31 +0200

    navbar finita

--- src/home/home.js
+++ src/home/home.js
@@ -2,9 +2,9 @@
 import Jumbo from '../Jumbo/jumbo';
 import AboutUs from '../about_us/about_us';
 import Services from '../services/services';
-import CarouselDiFra from "../carouselFra3/carousel";
 
-/* import App from "../carouselFra2/App"; */
+
+
 
 export default function Home() {
   /* var engb_text = getEngb_text(); */
@@ -13,8 +13,6 @@ export default function Home() {
       <Jumbo/>
       <AboutUs/>
       <Services/>
-      <CarouselDiFra/>
-      {/* <App/> */}
     </div>
   );
 };

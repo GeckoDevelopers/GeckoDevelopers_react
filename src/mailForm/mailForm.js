035f9119f57c83c893af65b8e2a454e081ba1925
France900 <francesco.em.livio@gmail.com>
2022-05-09 18:11:54 +0200

    errore sul form

--- src/mailForm/mailForm.js
+++ src/mailForm/mailForm.js
@@ -1,9 +1,23 @@
 import './mailForm.scss';
 import { getEngb_text } from "../lingue/inglese";
-import { Link } from "react-router-dom";
+import emailjs from 'emailjs-com';
 
 export default function MailForm() {
   var engb_text = getEngb_text();
+  /* const form = useRef(); */
+
+  function sendMail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'Gmail-eheheh', e.current, 's2f0n7Fyg964jaMFu')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      }
+    );
+    e.target.reset()
+  }
   
   return (
     <section id="contacts">
@@ -14,12 +28,28 @@ export default function MailForm() {
           <img src="./img/similmenu-white.svg" alt="" />
         </div>
 
-        <div className='mailForm'>
+        <form /* ref={form} */ onSubmit={sendMail}>
+          <div className="sinistra">
+            <label htmlFor="fname">Name</label><br/>
+            <input type="text" nome="nome"/><br/>
+            <label htmlFor="email">E-mail</label><br/>
+            <input type="email" nome="email"/><br/>
+            <label htmlFor="subject" >Subject</label><br/>
+            <input type="text" nome="oggetto"/><br/>
+          </div>
 
+          <div className="corpo-email">
+            <label htmlFor="subject">Text</label>
+            <textarea rows="12" cols="12" nome="message"></textarea>
+          </div>
 
-
-        </div>
-
+          <div className="sotto">
+            <div className="privacy">
+                <span>Informativa sulla privacy: l'email e il nome non verranno salvati, verranno utilizzati al solo scopo di comunicazioni azienda cliente</span><br/>
+            </div>
+            <input type="submit" value="Send"/>
+          </div>
+        </form>
 
         <p className="more-contacts">You can also find us here: <span>info@geckodevelopers.eu</span></p>
       </div>
@@ -27,3 +57,4 @@ export default function MailForm() {
 
   );
 };
+

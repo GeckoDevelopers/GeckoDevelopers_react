7cb88da8f2dd1b055b46d98880234bfd17c25476
France900 <francesco.em.livio@gmail.com>
2022-05-22 17:12:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter>
     <Navbar />
     <Routes>
       <Route path="/" element={<Home/>} />

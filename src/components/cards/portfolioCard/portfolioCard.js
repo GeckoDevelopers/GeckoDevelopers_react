f071c39df6abf50d085b6af5359a78b7481ae002
fabio98p <parisifabio98@gmail.com>
2022-09-15 18:32:34 +0200

    sistemato view portfolio

--- src/components/cards/portfolioCard/portfolioCard.js
+++ src/components/cards/portfolioCard/portfolioCard.js
@@ -1,31 +1,30 @@
-import './portfolioCard.scss';
-import { Link, useLocation } from "react-router-dom";
-import { useTranslation } from 'react-i18next';
+import './portfolioCard.scss'
+import { Link, useLocation } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
-export default function PortfolioCard({data}) {
-	const { t } = useTranslation();
+export default function PortfolioCard({ data }) {
+	const { t } = useTranslation()
 
-	const location = useLocation();
+	const location = useLocation()
 
-
-    function scrollUp() {
-        window.scrollTo(0, 0)
-     }
+	function scrollUp() {
+		window.scrollTo(0, 0)
+	}
 	// var classeee = location.pathname === "/" ? "grigiastro" : "verdastro" // funziona anche cosi
 	return (
-
-        <section id="portfolioCard" className={`${data.color}`}>
-            <div className="carta_container">
-                        <img src={`${data.image}`} alt="" />
-                <Link onClick={scrollUp} className="carta" to={`${data.link}`}>
-                    <div className="flex">
-                        <h3>{data.title}</h3>
-                    </div>
-                    <p>{data.paragrafo}</p>
-                    <span>{t('card_view_more')}</span>
-                </Link>
-            </div>
-        </section>
-        
-	);
-};
+		<section id="portfolioCard" className={`${data.color}`}>
+			<div className="carta_container">
+				<a target="_blank" href={`${data.link}`} className="carta">
+					<img src={`./img/portrait/${data.image1}`} alt="" />
+					<img src={`./img/portrait/${data.image2}`} alt="" />
+					<img src={`./img/portrait/${data.image3}`} alt="" />
+					<div className="flex">
+						<h3>{data.title}</h3>
+						<p>{data.paragrafo}</p>
+						<span>{t('card_view_more')}</span>
+					</div>
+				</a>
+			</div>
+		</section>
+	)
+}

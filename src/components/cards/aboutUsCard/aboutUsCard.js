fa6fc6471d85afdcf143b1404e6db4efabee7157
fabio98p <parisifabio98@gmail.com>
2022-09-15 17:49:14 +0200

    sistemato card aboutus

--- src/components/cards/aboutUsCard/aboutUsCard.js
+++ src/components/cards/aboutUsCard/aboutUsCard.js
@@ -1,31 +1,24 @@
-import './aboutUsCard.scss';
-import { Link, useLocation } from "react-router-dom";
-import { useTranslation } from 'react-i18next';
+import './aboutUsCard.scss'
+import { Link, useLocation } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 export default function AboutUsCard({ data }) {
-   const { t } = useTranslation();
+	const { t } = useTranslation()
 
-   const location = useLocation();
+	const location = useLocation()
 
-
-   function scrollUp() {
-      window.scrollTo(0, 0)
-   }
-   // var classeee = location.pathname === "/" ? "grigiastro" : "verdastro" // funziona anche cosi
-   return (
-
-      <section id="aboutUsCard" className={`${data.color}`}>
-         <div className="carta_container">
-            <img src={`${data.image}`} alt="" />
-            <Link onClick={scrollUp} className="carta" to={`${data.link}`}>
-               <div className="flex">
-                  <h3>{data.title}</h3>
-               </div>
-               <p>{data.paragrafo}</p>
-               <span>{t('card_view_more')}</span>
-            </Link>
-         </div>
-      </section>
-
-   );
-};       
+	// var classeee = location.pathname === "/" ? "grigiastro" : "verdastro" // funziona anche cosi
+	return (
+		<section id="aboutUsCard" className={`${data.color}`}>
+			<div className="carta_container">
+				<div className="carta">
+					<img src={`./img/portrait/${data.image}`} alt="" />
+					<div className="flex">
+						<h3>{data.title}</h3>
+						<p>{data.paragrafo}</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
